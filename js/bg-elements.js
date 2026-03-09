084b215af5ba3eff4cb61863d649a45a23151994
ruuuu <rufinka_91@mail.ru>
2026-03-09 18:36:37 +0300

    Add files via upload

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,6 +1,12 @@
-const elements = document.querySelectorAll('.set-bg')
+const bgElements = () => {
+
+  const elements = document.querySelectorAll('.set-bg')
 
 
-elements.forEach((elem) => {
-  elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
-})
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
+}
+
+
+bgElements();
